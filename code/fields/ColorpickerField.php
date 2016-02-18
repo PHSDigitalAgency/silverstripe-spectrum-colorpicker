748c5c7bbbb0f9cdcf404a5e8b54ecce98103875
Yves Trublin <yves@phsvietnam.com>
2016-02-18 11:31:27 +0700

    Update comments style

--- code/fields/ColorpickerField.php
+++ code/fields/ColorpickerField.php
@@ -3,39 +3,40 @@
 class ColorpickerField extends TextField
 {
     /**
-   * Spectrum Colorpicker Options
-   */
-  // 'color' => false,
-  // 'flat' => false,
-  // 'showInput' => false,
-  // 'allowEmpty' => false,
-  // 'showButtons' => true,
-  // 'clickoutFiresChange' => true,
-  // 'showInitial' => false,
-  // 'showPalette' => false,
-  // 'showPaletteOnly' => false,
-  // 'hideAfterPaletteSelect' => false,
-  // 'togglePaletteOnly' => false,
-  // 'showSelectionPalette' => true,
-  // 'localStorageKey' => false,
-  // 'appendTo' => "body",
-  // 'maxSelectionSize' => 7,
-  // 'cancelText' => "cancel",
-  // 'chooseText' => "choose",
-  // 'togglePaletteMoreText' => "more",
-  // 'togglePaletteLessText' => "less",
-  // 'clearText' => "Clear Color Selection",
-  // 'noColorSelectedText' => "No Color Selected",
-  // 'preferredFormat' => false,
-  // 'containerClassName' => "",
-  // 'replacerClassName' => "",
-  // 'showAlpha' => false,
-  // 'theme' => "sp-light",
-  // 'palette' => array("#ffffff", "#000000", "#ff0000", "#ff8000", "#ffff00", "#008000", "#0000ff", "#4b0082", "#9400d3"),
-  // 'selectionPalette' => [],
-  // 'disabled' => false,
-  // 'offset' => null
-
+     * Spectrum Colorpicker Options
+     *
+     * 'color' => false,
+     * 'flat' => false,
+     * 'showInput' => false,
+     * 'allowEmpty' => false,
+     * 'showButtons' => true,
+     * 'clickoutFiresChange' => true,
+     * 'showInitial' => false,
+     * 'showPalette' => false,
+     * 'showPaletteOnly' => false,
+     * 'hideAfterPaletteSelect' => false,
+     * 'togglePaletteOnly' => false,
+     * 'showSelectionPalette' => true,
+     * 'localStorageKey' => false,
+     * 'appendTo' => "body",
+     * 'maxSelectionSize' => 7,
+     * 'cancelText' => "cancel",
+     * 'chooseText' => "choose",
+     * 'togglePaletteMoreText' => "more",
+     * 'togglePaletteLessText' => "less",
+     * 'clearText' => "Clear Color Selection",
+     * 'noColorSelectedText' => "No Color Selected",
+     * 'preferredFormat' => false,
+     * 'containerClassName' => "",
+     * 'replacerClassName' => "",
+     * 'showAlpha' => false,
+     * 'theme' => "sp-light",
+     * 'palette' => array("#ffffff", "#000000", "#ff0000", "#ff8000", "#ffff00", "#008000", "#0000ff", "#4b0082", "#9400d3"),
+     * 'selectionPalette' => [],
+     * 'disabled' => false,
+     * 'offset' => null
+     **/
+ 
   public function __construct($name, $title = null, $value = '', $form = null)
   {
       parent::__construct($name, $title, $value);
